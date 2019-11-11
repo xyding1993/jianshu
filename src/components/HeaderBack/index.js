5a76416451b59e29198306cf088f9a37d78d4f3b
dingxiangyang <ding.xiangyang@ztesoft.com>
2019-11-11 15:56:31 +0800

    mod

--- src/components/HeaderBack/index.js
+++ src/components/HeaderBack/index.js
@@ -1,13 +1,5 @@
 import React, { Component } from "react";
-import {
-  HeaderWrapper,
-  Logo,
-  Nav,
-  NavItem,
-  NavSearch,
-  Addition,
-  Button
-} from "./style";
+import { HeaderWrapper, Logo, Nav, NavItem, NavSearch, Addition, Button } from "./style";
 
 class Header extends Component {
   render() {
@@ -18,12 +10,12 @@ class Header extends Component {
           <NavItem className="left active">首页</NavItem>
           <NavItem className="left">下载</NavItem>
           <NavSearch className="left"></NavSearch>
-          <NavItem className="right">登录</NavItem>
           <NavItem className="right">Aa</NavItem>
         </Nav>
         <Addition>
           <Button className="writer">写文章</Button>
-          <Button className="reg">注册</Button>
+          <Button className="sign">注册</Button>
+          <Button className="login">登录</Button>
         </Addition>
       </HeaderWrapper>
     );
