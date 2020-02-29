2480f12ce25f3aeeb9e9b83875640a5da349787a
esun <xydinga@163.com>
2020-02-29 11:34:40 +0800

    class

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -18,61 +18,66 @@ import {
   SearchInfoList
 } from "./style";
 
-const Header = props => {
-  const { focused, handlerInputFocus, handlerInputBlur } = props;
+class Header extends React.Component {
+  render() {
+    // command+d
+    const { focused, handlerInputFocus, handlerInputBlur } = this.props;
 
-  const getListArea = show => {
-    if(show) {
-      return (
-        <SearchInfo>
-          <SearchInfoTitle>
-            热门搜索
-            <SearchInfoSwitch>换一批</SearchInfoSwitch>
-          </SearchInfoTitle>
-          <SearchInfoList>
-            <SearchInfoItem>教育</SearchInfoItem>
-            <SearchInfoItem>教育</SearchInfoItem>
-            <SearchInfoItem>教育</SearchInfoItem>
-            <SearchInfoItem>教育</SearchInfoItem>
-            <SearchInfoItem>教育</SearchInfoItem>
-          </SearchInfoList>
-        </SearchInfo>
-      );
-    }
-    return null;
-  };
+    const getListArea = show => {
+      if (show) {
+        return (
+          <SearchInfo>
+            <SearchInfoTitle>
+              热门搜索
+              <SearchInfoSwitch>换一批</SearchInfoSwitch>
+            </SearchInfoTitle>
+            <SearchInfoList>
+              <SearchInfoItem>教育</SearchInfoItem>
+              <SearchInfoItem>教育</SearchInfoItem>
+              <SearchInfoItem>教育</SearchInfoItem>
+              <SearchInfoItem>教育</SearchInfoItem>
+              <SearchInfoItem>教育</SearchInfoItem>
+            </SearchInfoList>
+          </SearchInfo>
+        );
+      }
+      return null;
+    };
 
-  return (
-    <HeaderWrapper>
-      <Logo />
-      <Nav>
-        <NavItem className="left active">首页</NavItem>
-        <NavItem className="left">下载</NavItem>
-        <SearchWrapper>
-          <CSSTransition in={focused} timeout={200} classNames="slide">
-            <NavSearch
-              className={focused ? "focused" : ""}
-              onFocus={handlerInputFocus}
-              onBlur={handlerInputBlur}
-            ></NavSearch>
-          </CSSTransition>
-          <i className={focused ? "focused iconfont" : "iconfont"}>&#xe64d;</i>
-          {getListArea(focused)}
-        </SearchWrapper>
-        <NavItem className="right">登录</NavItem>
-        <NavItem className="right">Aa</NavItem>
-      </Nav>
-      <Addition>
-        <Button className="writer">写文章</Button>
-        <Button className="reg">注册</Button>
-      </Addition>
-    </HeaderWrapper>
-  );
-};
+    return (
+      <HeaderWrapper>
+        <Logo />
+        <Nav>
+          <NavItem className="left active">首页</NavItem>
+          <NavItem className="left">下载</NavItem>
+          <SearchWrapper>
+            <CSSTransition in={focused} timeout={200} classNames="slide">
+              <NavSearch
+                className={focused ? "focused" : ""}
+                onFocus={handlerInputFocus}
+                onBlur={handlerInputBlur}
+              ></NavSearch>
+            </CSSTransition>
+            <i className={focused ? "focused iconfont" : "iconfont"}>
+              &#xe64d;
+            </i>
+            {getListArea(focused)}
+          </SearchWrapper>
+          <NavItem className="right">登录</NavItem>
+          <NavItem className="right">Aa</NavItem>
+        </Nav>
+        <Addition>
+          <Button className="writer">写文章</Button>
+          <Button className="reg">注册</Button>
+        </Addition>
+      </HeaderWrapper>
+    );
+  }
+}
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
-    focused: state.getIn(['header', 'focused'])
+    focused: state.getIn(["header", "focused"])
   };
 };
 
