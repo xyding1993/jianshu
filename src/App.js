c5301fa1192e0b1a60c4891d9642c4cbd5e78189
esun <xydinga@163.com>
2020-02-29 15:59:14 +0800

    引入路由

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Globalstyle from "./style";
 import Globalstyle2 from "./static/iconfont/iconfont";
 import { Provider } from "react-redux";
 import store from "./store";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
@@ -11,10 +12,30 @@ function App() {
       <Globalstyle />
       <Globalstyle2 />
       <Provider store={store}>
-        <Header />
+        <div>
+          <Header />
+          <Router>
+            <Switch>
+              <Route path="/detail" exact>
+                <Detail />
+              </Route>
+              <Route path="/" exact>
+                <Home />
+              </Route>
+            </Switch>
+          </Router>
+        </div>
       </Provider>
     </div>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Detail() {
+  return <h2>Detail</h2>;
 }
 
 export default App;
