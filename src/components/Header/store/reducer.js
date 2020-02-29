0e5ff26ca907f82ef91d459824772e15b4f1990a
esun <xydinga@163.com>
2020-02-29 13:02:16 +0800

    代码简单优化

--- src/components/Header/store/reducer.js
+++ src/components/Header/store/reducer.js
@@ -7,16 +7,14 @@ const defaultState = fromJS({
 });
 
 export default (state = defaultState, action) => {
-  if (action.type === contants.SEARCH_FOCUS) {
-    return state.set("focused", true);
+  switch (action.type) {
+    case contants.SEARCH_FOCUS:
+      return state.set("focused", true);
+    case contants.SEARCH_BLUR:
+      return state.set("focused", false);
+    case contants.CHANGE_LIST:
+      return state.set("list", action.data);
+    default:
+      return state;
   }
-
-  if (action.type === contants.SEARCH_BLUR) {
-    return state.set("focused", false);
-  }
-
-  if (action.type === contants.CHANGE_LIST) {
-    return state.set("list", action.data);
-  }
-  return state;
 };
